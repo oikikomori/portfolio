f4512a754bc7a72b65c206c53aec6b2957ccffdc
오승일 <syoh@mysoftwiz.com>
2025-11-03 12:16:09 +0900

    빌드 오류

--- client/next.config.js
+++ client/next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     domains: ['localhost', 'kuuuma.com'],
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname),
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
